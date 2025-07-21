0c1d77b51ace694994cd7672669e2759f8196e46
rafa-houssam <rafahoussam4edu@gmail.com>
2025-07-21 18:44:54 +0100

    moving to the backend

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,11 +1,14 @@
 'use client'
-import  { useState } from 'react'
+import  { useEffect, useState } from 'react'
 import { products } from '../ProductData'
 import ProductsList from '../components/ProductsList'
 
-const Cart = () => {
+ async function Cart(){
     const [cartIds,setcartIds]=useState(['123','234'])
     const carProduct=cartIds.map(id=>products.find(p=>id==p.id)!)
+    const response=await fetch("/api/hello")
+    const data=response.json()
+    console.log(data)
   return (
     <>
     <div>shopping cart</div>
--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -5,18 +5,23 @@ import Image from 'next/image'
 
 
 const ProductDetails = ({ product, width, height, descr }: { product: Product, width: number, height: number, descr: boolean }) => {
+
     return (
         <>
            
 
 
-                <Link key={product.id} href={`/products/${product.id}`} className='text-center'>
-                   <div className={`w-${width} h-100px`}>
-                    <Image className='w-full h-full ' src={product.imageUrl} alt="Product image" width={width} height={100}></Image></div>
+                <Link key={product.id} href={`/products/${product.id}`} className='text-center  '>
+                   {descr && (
+                    <Image className='ml-90 m-2 mt-4 ' src={product.imageUrl} alt="Product image" width={width} height={height}></Image>)}
+                    {!descr && (<div className={`w-${width} h-100px`}>
+                    <Image className='w-full h-full m-2  ' src={product.imageUrl} alt="Product image" width={width} height={100}></Image></div>)}
                     <h2>{product.name}</h2>
                     <h2>${product.price}</h2>
+                    {/* <h1 className='text-bold text-2xl'>Description</h1> */}
                     {descr && (
-                        <p className=''>{product.description}</p>
+                        <>
+                        <p className=''>{product.description}</p></>
                     )}
                 </Link>
            
