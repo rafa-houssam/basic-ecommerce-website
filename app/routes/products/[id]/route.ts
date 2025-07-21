531bab3cb9af3e7bee2a9843d640c8c954394045
rafa-houssam <rafahoussam4edu@gmail.com>
2025-07-21 21:09:56 +0100

    cart route

--- app/routes/products/[id]/route.ts
+++ app/routes/products/[id]/route.ts
@@ -7,6 +7,9 @@ type Params={
 export async function GET(request:NextRequest,{params}:{params:Params}){
     const productId=params.id
     const product=products.find(p=>p.id==productId)
+    if(!product){
+        return NextResponse.json({"error":"Product not found"},{status:404})
+    }
     return NextResponse.json({product},{status:200})
 
 
---
+++ app/routes/users/[id]/cart/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server"
+import { products } from "@/app/ProductData"
+
+
+type Params={
+    id:string
+}
+type ShoppingCart=Record<string,string[]>
+const carts:ShoppingCart={
+    '1':['123','234'],
+    '2':['345','456'],
+    '3':['123'],
+}
+
+export async function GET(request:NextRequest,{params}:{params:Params}){
+    const userId=params.id
+    const productIds=carts[userId]
+    const userProducts=productIds.map(id=>products.find(p=>p.id==id))
+    return NextResponse.json({userProducts},{status:200})
+
+    
+
+
+
+}
